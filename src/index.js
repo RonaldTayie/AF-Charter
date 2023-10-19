ed3ef1b4e8d4e52e9ae11c599549e7c2b17a33af
ronalddsf <ronaldn@digitalsolutionfoundry.co.za>
2023-10-19 15:03:36 +0200

    update

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ const drawChart = require("./helpers/drawChart");
 
 app.use(express.json());
 app.post("/api/chart", async (req, res) => {
-    var data = req.body;
-    const chart = await drawChart(data);
-    res.setHeader("Content-Type", "image/png");
-    res.setHeader("Content-Disposition", "attachment; filename=chart.png");
-    res.send(chart);
+    try{
+        var data = req.body;
+        const chart = await drawChart(data);
+        res.setHeader("Content-Type", "image/png");
+        res.setHeader("Content-Disposition", "attachment; filename=chart.png");
+        res.send(chart);
+    }catch (e) {
+        console.log(e)
+    }
 });
 
 const port = 3001;
